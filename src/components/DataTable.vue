6f603fd94c1de04e438624674bd2d6c11a1616f7
ya9 <devyassine47@gmail.com>
2023-01-25 17:40:00 +0100

    added pagination with raw data

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -5,13 +5,14 @@
             <v-grid
                 :can-focus="false"
                 theme="material"
-                :source="source"
+                :source="rows"
                 :columns="columns"
                 :filter="filterConfig"
                 :resize="true"
+                :range="3"
                 :sortable="true"
                 @beforefiltertrimmed="aftertrimmed"
-                frameSize="5"
+                frameSize="2"
                 class="tw-border"
             >
             </v-grid>
@@ -37,6 +38,8 @@
 import VGrid from '@revolist/vue3-datagrid'
 import filterConfig from '@/config/dataTableFilters'
 
+
+
 export default {
     components: { VGrid },
     props: {
@@ -55,26 +58,77 @@ export default {
     data() {
         return {
             isFiltered: false,
-            source: this.rows,
+            // filtredItems: [],
+            // source: [],
             currentPage: 1,
-            paginationLimit: 5,
+            paginationLimit: 7,
             filtredProducts: [],
+            filtredOut: {}
         }
     },
 
     computed: {
         pageCount() {
-            return Math.ceil(this.source.length / this.paginationLimit)
+            return Math.ceil(this.rows.length / this.paginationLimit)
+        },
+        prevRange() {
+            return (this.currentPage - 1) * this.paginationLimit
+        },
+        nextRange() {
+            return (this.currentPage) * this.paginationLimit
+        },
+        source() {
+            return this.isFiltered ? [] : this.rows.slice(this.prevRange, this.nextRange);
+        },
+        filtredItems() {
+            return this.rows.filter((item, index) => this.filtredOut.includes(index))
+        },
+
+
+        trimmedNoFilter() {
+            const trimmed = {}
+
+            this.rows.forEach((e, i) => {
+                if(i < this.prevRange || i > this.nextRange - 1) {
+                    trimmed[i] = true
+                }
+            })
+
+            return trimmed
+        },
+
+        trimmedWithFilter() {
+            let trimmed = {...this.filtredOut}
+
+            return trimmed
+        }
+    },
+
+    watch: {
+        currentPage() {
+            document.querySelector('revo-grid').addTrimmed(this.isFiltered ? this.trimmedWithFilter : this.trimmedNoFilter)
+            console.log(this.trimmedNoFilter);
+        },
+        filtredOut() {
+            document.querySelector('revo-grid').addTrimmed(this.isFiltered ? this.trimmedWithFilter : this.trimmedNoFilter)
         }
     },
 
     methods: {
         aftertrimmed(source) {
-            this.isFiltered = Object.keys(source.detail.collection).length > 0;
-            // source.preventDefault();
+            const filtersCount = Object.keys(source.detail.collection).length
+            if (filtersCount > 0) {
+                this.filtredOut = source.detail.itemsToFilter
+                this.isFiltered =  true;
+                
+            } else {
+                this.filtredOut = {}
+                this.isFiltered = false
+            }
             // const newRows = Object.keys(source.detail.itemsToFilter);
             console.log(source);
-            console.log(this.isFiltered);
+            // console.log(this.isFiltered);
+        
             // console.log(Object.keys(source.detail.itemsToFilter));
             // const filtredRows = this.rows.filter((e,i) => !newRows.includes(`${i}`));
         }
@@ -82,6 +136,11 @@ export default {
 
     mounted() {
         console.log(this.pageCount);
+        console.log(this.prevRange);
+        console.log(this.nextRange);
+            
+        document.querySelector('revo-grid').addTrimmed(this.isFiltered ? this.trimmedWithFilter : this.trimmedNoFilter)
+
     }
 }
 </script>
--- src/views/product/config.js
+++ src/views/product/config.js
@@ -72,5 +72,151 @@ export const rows =
         sku: 'P-124',
         category: 'computer',
         status: 'pending'
+    },
+
+    {
+        id: 9,
+        name: "Product name 1",
+        price: 12.00,
+        qty: 100,
+        sku: 'P-121',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 10,
+        name: "Product name 2",
+        price: 22.00,
+        qty: 130,
+        sku: 'P-122',
+        category: 'computer',
+        status: 'outOfStock'
+    },
+    {
+        id: 11,
+        name: "Product name 3",
+        price: 32.00,
+        qty: 500,
+        sku: 'P-123',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 12,
+        name: "Product name 4",
+        price: 42.00,
+        qty: 230,
+        sku: 'P-124',
+        category: 'computer',
+        status: 'pending'
+    },
+    {
+        id: 13,
+        name: "Product name 5",
+        price: 12.00,
+        qty: 100,
+        sku: 'P-125',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 14,
+        name: "Product name 6",
+        price: 22.00,
+        qty: 130,
+        sku: 'P-122',
+        category: 'computer',
+        status: 'outOfStock'
+    },
+    {
+        id: 15,
+        name: "Product name 7",
+        price: 32.00,
+        qty: 500,
+        sku: 'P-123',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 16,
+        name: "Product name 8",
+        price: 42.00,
+        qty: 230,
+        sku: 'P-124',
+        category: 'computer',
+        status: 'pending'
+    },
+
+    {
+        id: 17,
+        name: "Product name 1",
+        price: 12.00,
+        qty: 100,
+        sku: 'P-121',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 18,
+        name: "Product name 2",
+        price: 22.00,
+        qty: 130,
+        sku: 'P-122',
+        category: 'computer',
+        status: 'outOfStock'
+    },
+    {
+        id: 19,
+        name: "Product name 3",
+        price: 32.00,
+        qty: 500,
+        sku: 'P-123',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 20,
+        name: "Product name 4",
+        price: 42.00,
+        qty: 230,
+        sku: 'P-124',
+        category: 'computer',
+        status: 'pending'
+    },
+    {
+        id: 21,
+        name: "Product name 5",
+        price: 12.00,
+        qty: 100,
+        sku: 'P-125',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 22,
+        name: "Product name 6",
+        price: 22.00,
+        qty: 130,
+        sku: 'P-122',
+        category: 'computer',
+        status: 'outOfStock'
+    },
+    {
+        id: 23,
+        name: "Product name 7",
+        price: 32.00,
+        qty: 500,
+        sku: 'P-123',
+        category: 'computer',
+        status: 'inShop'
+    },
+    {
+        id: 24,
+        name: "Product name 8",
+        price: 42.00,
+        qty: 230,
+        sku: 'P-124',
+        category: 'computer',
+        status: 'pending'
     }
   ]
