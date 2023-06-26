30893fa66c33c79b3763bf163fb097b39fb1120b
Abdelmounaim <oulaidabdelmonaim2@gmail.com>
2023-06-26 18:44:47 +0100

    'SalesAnalytic'

--- src/views/sale/filters/filter.js
+++ src/views/sale/filters/filter.js
@@ -1,153 +1,182 @@
-export default function filteredSales(sales, search, {
+export default function filteredSales(
+  sales,
+  search,
+  {
     confirmationFilter,
     affectationFilter,
     upsellFilter,
     deliveryFilter,
     agenteFilter,
     dateFilter,
-    productFilter
-}) {
+    productFilter,
+  }
+) {
+  const startDate = dateFilter[0];
+  const endDate = dateFilter[1];
 
-    const startDate = dateFilter[0];
-    const endDate = dateFilter[1];
-    
-    return sales.filter(item => {
-        const createdAt = new Date(item.created_at);
+  return sales.filter((item) => {
+    const createdAt = new Date(item.created_at);
 
-        // filter by confirmation
-        if (confirmationFilter !== 'all' && item.confirmation !== confirmationFilter) {
-            return false;
-        }
+    // filter by confirmation
+    if (
+      confirmationFilter !== "all" &&
+      item.confirmation !== confirmationFilter
+    ) {
+      return false;
+    }
 
-        // filter by affectation
-        if (affectationFilter !== 'all' && item.affectation != affectationFilter) {
-            return false;
-        }
+    // filter by affectation
+    if (affectationFilter !== "all" && item.affectation != affectationFilter) {
+      return false;
+    }
 
-        // filter by upsell
-        if (upsellFilter !== 'all' && item.upsell != upsellFilter) {
-            return false;
-        }
+    // filter by upsell
+    if (upsellFilter !== "all" && item.upsell != upsellFilter) {
+      return false;
+    }
 
-        // filter by delivery
-        if (deliveryFilter !== 'all' && item.delivery != deliveryFilter) {
-            return false;
-        }
+    // filter by delivery
+    if (deliveryFilter !== "all" && item.delivery != deliveryFilter) {
+      return false;
+    }
 
-        // filter by agente
-        if (agenteFilter !== 'all' && item.agente_id != agenteFilter) {
-            return false;
-        }
+    // filter by agente
+    if (agenteFilter !== "all" && item.agente_id != agenteFilter) {
+      return false;
+    }
 
-        if (!item.fullname.toLowerCase().includes(search.toLowerCase())) {
-            return false;
-        }
+    if (!item.fullname.toLowerCase().includes(search.toLowerCase())) {
+      return false;
+    }
 
-        if(productFilter !== 'all' && !item.items.some(i => i.product_id == productFilter)) {
-            return false;
-        }
+    if (
+      productFilter !== "all" &&
+      !item.items.some((i) => i.product_id == productFilter)
+    ) {
+      return false;
+    }
 
-        // if (!item.city.toLowerCase().includes(search.toLowerCase())) {
-        //     return false;
-        // }
+    // if (!item.city.toLowerCase().includes(search.toLowerCase())) {
+    //     return false;
+    // }
 
-        /* eslint-disable */
-        const createdAtDay = createdAt.getDate();
-        const createdAtMonth = createdAt.getMonth();
-        const createdAtYear = createdAt.getFullYear();
+    /* eslint-disable */
+    const createdAtDay = createdAt.getDate();
+    const createdAtMonth = createdAt.getMonth();
+    const createdAtYear = createdAt.getFullYear();
 
-        if (!!startDate) {
-            const startDay = startDate.getDate();
-            const startMonth = startDate.getMonth();
-            const startYear = startDate.getFullYear();
+    if (!!startDate) {
+      const startDay = startDate.getDate();
+      const startMonth = startDate.getMonth();
+      const startYear = startDate.getFullYear();
 
-            if (
-                createdAtYear < startYear ||
-                (createdAtYear === startYear && createdAtMonth < startMonth) ||
-                (createdAtYear === startYear && createdAtMonth === startMonth && createdAtDay < startDay)
-            ) {
-                return false;
-            }
-        }
+      if (
+        createdAtYear < startYear ||
+        (createdAtYear === startYear && createdAtMonth < startMonth) ||
+        (createdAtYear === startYear &&
+          createdAtMonth === startMonth &&
+          createdAtDay < startDay)
+      ) {
+        return false;
+      }
+    }
 
+    if (!!endDate) {
+      const endDay = endDate.getDate();
+      const endMonth = endDate.getMonth();
+      const endYear = endDate.getFullYear();
 
-        if (!!endDate) {
-            const endDay = endDate.getDate();
-            const endMonth = endDate.getMonth();
-            const endYear = endDate.getFullYear();
+      if (
+        createdAtYear > endYear ||
+        (createdAtYear === endYear && createdAtMonth > endMonth) ||
+        (createdAtYear === endYear &&
+          createdAtMonth === endMonth &&
+          createdAtDay > endDay)
+      ) {
+        return false;
+      }
+    }
 
-            if (
-                createdAtYear > endYear ||
-                (createdAtYear === endYear && createdAtMonth > endMonth) ||
-                (createdAtYear === endYear && createdAtMonth === endMonth && createdAtDay > endDay)
-            ) {
-                return false;
-            }
-        }
-
-        // if item passes all filters, include it in the filtered data
-        return true;
-    });
+    // if item passes all filters, include it in the filtered data
+    return true;
+  });
 }
-
 
 export function newSales(sales) {
-    return {
-        id: 1,
-        title: "New",
-        value: sales.filter(
-            (i) => !i.confirmation || i.confirmation == "new"
-        ).length,
-        // value: 35,
-        color: "primary-green",
-        icon: "mdi mdi-new-box",
-    };
+  return {
+    id: 1,
+    title: "New",
+    value: sales.filter((i) => !i.confirmation || i.confirmation == "new")
+      .length,
+    // value: 35,
+    // pourcentage: (
+    //   (sales.filter((i) => !i.confirmation || i.confirmation == "new").length *
+    //     100) /
+    //   this.sales.length
+    // ).toFixed(2),
+    pourcentage: ((sales.filter((i) => !i.confirmation || i.confirmation == "new").length * 100 ) / sales.length ).toFixed(2),
+    color: "primary-green",
+    icon: "mdi mdi-new-box",
+  };
 }
 
-export function toProcessSales(sales) {
-    return {
-        id: 2,
-        title: "Process",
-        value: sales.filter(
-            (i) =>
-            !!i.confirmation &&
-            !["confirmer", "livre", "expidier"].includes(i.confirmation)
-        ).length,
-        // value: 231,
-        color: "primary-orange",
-        icon: "mdi-reload",
-    };
-}
+// export function toProcessSales(sales) {
+//   return {
+//     id: 2,
+//     title: "Process",
+//     value: sales.filter(
+//       (i) =>
+//         !!i.confirmation &&
+//         !["confirmer", "livre", "expidier"].includes(i.confirmation)
+//     ).length,
+//     // value: 231,
+//     color: "primary-orange",
+//     icon: "mdi-reload",
+//   };
+// }
 
 export function confirmedSales(sales) {
-    return {
-        id: 3,
-        title: "Confirmed",
-        value: sales.filter((i) => i.confirmation == "confirmer").length,
-        // value: 112,
-        color: "deep-purple-accent-2",
-        icon: "mdi-phone-check",
-    }
+  return {
+    id: 3,
+    title: "Confirmed",
+    value: sales.filter((i) => i.confirmation == "confirmer").length,
+    pourcentage: (
+        (sales.filter((i) => i.confirmation == "confirmer").length*
+          100) /
+        sales.length
+      ).toFixed(2),
+    // value: 112,
+    color: "deep-purple-accent-2",
+    icon: "mdi-phone-check",
+  };
 }
 
 export function deliveredSales(sales) {
-    return {
-        id: 4,
-        title: "Delivered",
-        value: sales.filter((i) => i.delivery == "livrer").length,
-        // value: 412,
-        color: "primary-blue",
-        icon: "mdi-account-check-outline",
-    };
+  return {
+    id: 4,
+    title: "Delivered",
+    value: sales.filter((i) => i.delivery == "livrer").length,
+    pourcentage: (
+        (sales.filter((i) => i.delivery == "livrer").length*
+          100) /
+          sales.filter((i) => i.confirmation == "confirmer").length
+      ).toFixed(2),
+    color: "primary-blue",
+    icon: "mdi-account-check-outline",
+  };
 }
 
 export function shippedSales(sales) {
-    return {
-        id: 5,
-        title: "Shipped",
-        value: sales.filter((i) => i.delivery == "expidier").length,
-        // value: 112,
-        color: "red",
-        icon: "mdi-truck",
-    };
+  return {
+    id: 5,
+    title: "Shipped",
+    value: sales.filter((i) => i.delivery == "expidier").length,
+    pourcentage: (
+        (sales.filter((i) => i.delivery == "expidier").length*
+          100) /
+          sales.filter((i) => i.confirmation == "confirmer").length
+      ).toFixed(2),
+    color: "red",
+    icon: "mdi-truck",
+  };
 }
--- src/views/sale/partials/DashItem.vue
+++ src/views/sale/partials/DashItem.vue
@@ -5,19 +5,25 @@
         height="70"
     >
 
-        <div class="tw-flex tw-justify-between tw-items-center tw-h-full pa-3 px-4">
-            <div class="tw-flex tw-flex-col tw-justify-center tw-h-full tw-text-white">
+        <div class="tw-flex tw-justify-between tw-items-center tw-w-full tw-h-full pa-3 px-4">
+            <div class="tw-flex tw-flex-col tw-justify-center tw-w-full tw-h-full tw-text-white">
                 <p class="tw-text-xl tw-font-bold">
-                    <number
-                        ref="number1"
-                        :from="0"
-                        :to="dash.value"
-                        :duration="1"
-                        :delay="0"
-                        easing="Power.easeOut"/>
+                    {{ isNaN(dash.pourcentage) ? '0.00' : dash.pourcentage }} {{ symbol }}
                 </p>
                 <!-- <v-spacer></v-spacer> -->
-                <p class="tw-text-sm">{{ dash.title }}</p>
+                <span class="tw-flex tw-items-center tw-gap-1 tw-w-full tw-h-full tw-text-white">
+                    <number
+                    v-if="numbers"
+                    class="tw-font-bold"
+                    ref="number1"
+                    :from="0"
+                    :to="dash.value"
+                    :duration="1"
+                    :delay="0"
+                    easing="Power.easeOut"/>
+                    <p class="tw-text-sm">{{ dash.title }}</p>
+                    <!-- <span class="tw-text-sm tw-block tw-font-bold tw-pr-5">100%</span> -->
+                </span>
             </div>
             <div class="rounded-circle tw-w-fit tw-mr-3">
                 <v-icon color="white" size="large" class="md:tw-scale-[1.8] tw-scale-[1.3]" :icon="dash.icon"></v-icon>
@@ -30,7 +36,19 @@
 
 <script>
 export default {
-    props: ['dash']
+    props: {
+        dash: {
+            required: true
+        },
+        symbol: {
+            required: false,
+            default: '%'
+        },
+        numbers : {
+            required : false,
+            default : true
+        }
+    }
 }
 </script>
 
