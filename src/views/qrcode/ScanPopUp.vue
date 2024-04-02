730b126c77a616be877274b1904e01c4322e7960
GG-480_O <omnigold450@gmail.com>
2024-04-02 12:45:54 +0000

    add audio

--- src/views/qrcode/ScanPopUp.vue
+++ src/views/qrcode/ScanPopUp.vue
@@ -1,35 +1,60 @@
 <template >
-    <div>
+    <div :add="audiourl = $frontend('assets/bip.mp3')">
         <button @click="togglePopUp(true)" class="tw-flex tw-items-center tw-gap-2 tw-py-2 tw-px-4 tw-bg-white hover:tw-bg-neutral-100 tw-duration-200 tw-border tw-border-solid tw-border-neutral-200 tw-rounded-md tw-mb-2">
             <icon icon="iconamoon:scanner-light" class="tw-text-xl" />
             <span>Scan</span>
         </button>
 
         <popup-new :visible="props.visible"  >
-            <div class="">
-        <qrcode-stream
-            :constraints="{ deviceId: selectedDevice.deviceId }"
-            :track="trackFunctionSelected.value"
-            :formats="selectedBarcodeFormats"
-            @error="onError"
-            @detect="onDetect"
-            v-if="selectedDevice !== null"
-        ></qrcode-stream>
-        <p
-            v-else
-            class="error"
-        >
-            No cameras on this device
-        </p>
+            <div class="tw-w-full tw-max-w-[600px] tw-mx-auto tw-p-2">
+            <div class="tw-w-full tw-h-fit tw-bg-white tw-rounded-md tw-border tw-border-solid tw-shadow-md">
+                <h1
+                        class="tw-text-lg tw-font-medium tw-text-gray-600 tw-bg-gray-100 tw-border-b tw-border-solid tw-p-4">
+                        Scan the QR code 
+                    </h1>
+                <div class="tw-p-5">
+
+                    <div class="tw-rounded tw-overflow-hidden tw-ring tw-ring-orange-400">
+
+                        <qrcode-stream
+                        :constraints="{ deviceId: selectedDevice.deviceId }"
+                        :track="trackFunctionSelected.value"
+                        :formats="selectedBarcodeFormats"
+                        @error="onError"
+                        @detect="onDetect"
+                        v-if="selectedDevice !== null"
+                        ></qrcode-stream>
+                        <p
+                        v-else
+                        class="error"
+                        >
+                        No cameras on this device
+                    </p>
+                </div>
+
+            </div>
+            <div
+                        class="tw-flex tw-justify-end tw-items-center tw-p-2 tw-gap-3 dark:tw-bg-neutral-900 tw-bg-gray-100 tw-border-t tw-border-solid">
+                        <button 
+                            @click="togglePopUp(false)"
+                            :disabled="false"
+                            class="tw-py-2 tw-px-7 tw-rounded tw-text-sm tw-border tw-border-solid tw-border-tansparent dark:tw-border-neutral-900 hover:tw-border-neutral-400 dark:hover:tw-border-neutral-500 hover:tw-bg-gray-300 tw-bg-gray-200 dark:tw-bg-neutral-600 tw-duration-300 tw-text-neutral-900 dark:tw-text-neutral-300">
+                            Cancel
+                        </button>
+                       
+                    </div>
     </div>
+</div>
     </popup-new>
     </div>
 </template>
 <script setup>
 import { QrcodeStream } from 'vue-qrcode-reader'
 import {ref,computed,onMounted, defineEmits, defineProps} from "vue";
+import { useAlert } from '@/composables/useAlert';
 
-const emit = defineEmits(['update:visible'])
+const audiourl = ref('')
+const emit = defineEmits(['update:visible',"detect"])
 const props = defineProps(['visible'])
 const togglePopUp = (value) => { 
     emit("update:visible",value)
@@ -106,6 +131,11 @@ const result = ref('')
 
 function onDetect(detectedCodes) {
     const numbersonly = detectedCodes.map((code) => code.rawValue.replace(/[^0-9]/g, '')).join(', ');
+    useAlert('Your order ID is: ' + numbersonly)
+    emit("detect",numbersonly);
+    togglePopUp(false);
+    var audio = new Audio(audiourl.value);
+    audio.play();
     result.value = numbersonly;
 }
 const barcodeFormats = ref({
--- src/views/qrcode/ScannerView.vue
+++ src/views/qrcode/ScannerView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ScanPopup v-model:visible="showPopupShow" />
+        <ScanPopup v-model:visible="showPopupShow" @detect="handleDetect" />
     </div>
     <div class="py-5 px-2 tw-border tw-h-[calc(100vh-150px)] tw-bg-white tw-w-full tw-rounded-md tw-mb-5" >
         <div class="tw-grid tw-px-2 tw-grid-cols-12 tw-justify-center tw-h-full ">
@@ -15,7 +15,7 @@
                     <hr class="tw-flex-grow tw-border-t tw-border-gray-600 tw-w-[60px]"> 
                 </div>    
                 <div class="tw-grid tw-grid-cols-8 col-span-12 tw-gap-2 tw-w-full tw-max-w-[23rem]">
-                    <input id="code" type="text"  class="tw-bg-gray-50 border tw-col-span-6 tw-rounded-lg focus:tw-ring-gray-500 focus:tw-border-gray-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-gray-400 dark:tw-focus:tw-ring-blue-500 dark:focus:tw-border-blue-500">
+                    <input id="code" type="text" v-model="id"  class="tw-bg-gray-50 border tw-col-span-6 tw-rounded-lg focus:tw-ring-gray-500 focus:tw-border-gray-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-gray-400 dark:tw-focus:tw-ring-blue-500 dark:focus:tw-border-blue-500">
                     <button @click='Copy' class="tw-col-span-2 tw-text-white tw-bg-gray-700 tw-hover:bg-blue-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-gray-300 tw-font-medium tw-rounded-lg tw-text-sm w-full sm:tw-w-auto tw-py-2.5 tw-text-center dark:tw-bg-blue-600 dark:hover:tw-bg-blue-700 dark:focus:tw-ring-blue-800 tw-items-center tw-inline-flex tw-justify-center">
                         <span id="success-message" class="tw-inline-flex tw-items-center">
                             <icon icon="mdi:content-copy"/>
@@ -40,7 +40,11 @@
 <script setup>
 import {ref} from "vue";
 import ScanPopup from "./ScanPopUp.vue";
-    const showPopupShow = ref(false)
+    const showPopupShow = ref(false);
+    const id=ref("");
+    const handleDetect = (v) => { 
+        id.value = v;
+     }
     const Copy = () => { 
         var copyText = document.getElementById("code");
         copyText.select();
