eed57dde5ee05fe91bec13a4740c337a012664ce
ya9 <devyassine47@gmail.com>
2024-04-25 17:35:23 +0100

    added treated at, total to ads tables

--- src/views/ads/partials/table/TableRowTotal.vue
+++ src/views/ads/partials/table/TableRowTotal.vue
@@ -1,5 +1,5 @@
 <template>
-    <tr class="tw-bg-gray-50 tw-border-t tw-border-gray-300">
+    <tr class="tw-bg-gray-50 tw-border-t tw-border-gray-300 tw-sticky tw-bottom-0">
       <td
         class="tw-px-4 tw-py-2 tw-text-sm tw-font-medium tw-whitespace-nowrap"
       >
@@ -24,28 +24,36 @@
       <td
         class="tw-px-4 tw-py-2 tw-text-sm tw-font-medium tw-whitespace-nowrap"
       >
-        <div>
-          <h2 class="tw-font-bold tw-text-green-500 darkx:tw-text-white tw-font-[cairo]">
-            {{ currency }} {{ totalSpend }}
-          </h2>
+        <div class="tw-flex tw-flex-col tw-items-start tw-gap-1">
+            <h2 class="tw-font-bold tw-text-green-500 darkx:tw-text-white tw-font-[cairo]">
+                {{ currency }}{{ totalSpend }}
+            </h2>
+            <p class="tw-text-xs tw-text-green-500x tw-font-semibold">TOTAL SPEND</p>
         </div>
       </td>
       <td
         class="tw-px-4 tw-py-2 tw-text-sm tw-font-medium tw-whitespace-nowrap"
       >
-        <div class="tw-flex tw-items-center tw-gap-2">
-          <icon icon="material-symbols-light:leaderboard-rounded" class="tw-text-lg tw-text-cyan-400" />
-          <h2 class="tw-font-bold tw-text-cyan-500 darkx:tw-text-white">
-            {{ totalLeads }}
-          </h2>
+        <div class="tw-flex tw-flex-col tw-items-start tw-gap-1">
+            <div class="tw-flex tw-items-center tw-gap-2">
+                <icon icon="material-symbols-light:leaderboard-rounded" class="tw-text-lg tw-text-cyan-400" />
+                <h2 class="tw-font-bold tw-text-cyan-500 darkx:tw-text-white">
+                    {{ totalLeads }}
+                </h2>
+            </div>
+            <p class="tw-text-xs tw-text-cyan-500x tw-font-semibold">TOTAL LEADS</p>
         </div>
       </td>
  
       <td
       class="tw-px-4 tw-py-2 tw-text-sm tw-font-medium tw-whitespace-nowrap"
     >
-      <div>
-      </div>
+    <div class="tw-flex tw-items-start tw-flex-col tw-gap-1">
+        <div class="tw-flex tw-items-center tw-gap-1">
+            <span class="tw-font-bold tw-text-fuchsia-500">{{ currency }}{{ ( totalSpend / totalLeads).toFixed(2) }}</span>
+        </div>
+        <p class="tw-text-xs tw-text-fuchsia-500x tw-font-semibold">COST PER LEAD</p>
+    </div>
     </td>
       <td class="tw-px-4 tw-py-2 tw-text-sm tw-whitespace-nowrap">
         <div>
