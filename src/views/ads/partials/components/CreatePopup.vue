f4fbbdf2da8370ebe7f7d9e1c9c657552a9f000a
GG-480_O <omnigold450@gmail.com>
2024-06-29 13:41:42 +0100

    create ads multiple

--- src/views/ads/partials/components/CreatePopup.vue
+++ src/views/ads/partials/components/CreatePopup.vue
@@ -13,7 +13,7 @@
             <div class="tw-col-span-2 md:tw-col-span-1">
               <label
                 class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white">Source</label>
-              <select @change="errors.source = null" v-model="items.source"
+              <select @change="errors[0].source = null" v-model="items[0].source"
                 :class="[errors.source && '!tw-border-red-400']" required
                 class="tw-capitalize tw-bg-gray-50 tw-border tw-border-solid focus:tw-outline-none tw-border-gray-300 tw-text-gray-900 tw-text-sm tw-rounded-lg focus:tw-ring-orange-500 focus:tw-border-orange-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-orange-500 dark:focus:tw-border-orange-500">
                 <option :value="0" selected>Choose Source</option>
@@ -29,7 +29,7 @@
             <div class="tw-col-span-2 md:tw-col-span-1">
               <label
                 class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white">Amount</label>
-              <input v-model="items.amount" @input="errors.amount = null"
+              <input v-model="items[0].amount" @input="errors[0].amount = null"
                 :class="[errors.amount && '!tw-border-red-400']" type="text"
                 class="tw-bg-gray-50 tw-border tw-border-solid focus:tw-outline-none tw-border-gray-300 tw-text-gray-900 tw-text-sm tw-rounded-lg focus:tw-ring-orange-500 focus:tw-border-orange-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-orange-500 dark:focus:tw-border-orange-500"
                 required />
@@ -42,7 +42,7 @@
             <div class="tw-col-span-2 md:tw-col-span-1">
               <label
                 class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white">Leads</label>
-              <input v-model="items.leads" @input="errors.leads = null"
+              <input v-model="items[0].leads" @input="errors[0].leads = null"
                 :class="[errors.leads && '!tw-border-red-400']" type="text"
                 class="tw-bg-gray-50 tw-border tw-border-solid focus:tw-outline-none tw-border-gray-300 tw-text-gray-900 tw-text-sm tw-rounded-lg focus:tw-ring-orange-500 focus:tw-border-orange-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-orange-500 dark:focus:tw-border-orange-500"
                 required />
@@ -57,9 +57,9 @@
                 class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white">Product</label>
         
               <vue-select :reduce="(o) => o.id" @option:selected="e=> items.product_id = e.id"  :clearable="false" class="tw-bg-gray-50 tw-border-solid tw-outline-none  tw-text-gray-900 tw-text-sm tw-rounded-lg focus:tw-ring-orange-500 focus:tw-border-orange-500 tw-block tw-w-full"
-                  placeholder="All" v-model="items.product_id" :options="products" label="name">
+                  placeholder="All" v-model="items[0].product_id" :options="products" label="name">
                 </vue-select>
-              <label v-if="errors.product_id"
+              <label v-if="errors[0].product_id"
                 class="tw-block tw-mb-2 tw-text-xs tw-font-medium tw-text-red-400 dark:tw-text-white">{{
                 errors.product_id }}</label>
             </div>
@@ -67,7 +67,7 @@
             <div v-if="user.role == 'admin' " class="tw-col-span-2 md:tw-col-span-1">
               <label
                 class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white">Marketer </label>
-              <select @change="errors.marketer_id = null" v-model="items.marketer_id"
+              <select @change="errors[0].marketer_id = null" v-model="items[0].marketer_id"
                 :class="[errors.marketer_id && '!tw-border-red-400']" required
                 class="tw-bg-gray-50 tw-border tw-border-solid focus:tw-outline-none tw-border-gray-300 tw-text-gray-900 tw-text-sm tw-rounded-lg focus:tw-ring-orange-500 focus:tw-border-orange-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-orange-500 dark:focus:tw-border-orange-500">
                 <option :value="0" selected>Choose Marketer</option>
@@ -83,7 +83,7 @@
             <div class="md:tw-col-span-1 tw-col-span-2">
               <label class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white">Ads
                 Date</label>
-              <input v-model="items.ads_at" @input="errors.ads_at = null"
+              <input v-model="items[0].ads_at" @input="errors[0].ads_at = null"
                 :class="[errors.ads_at && '!tw-border-red-400']" type="date"
                 class="tw-bg-gray-50 tw-border tw-border-solid focus:tw-outline-none tw-border-gray-300 tw-text-gray-900 tw-text-sm tw-rounded-lg focus:tw-ring-orange-500 focus:tw-border-orange-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-orange-500 dark:focus:tw-border-orange-500"
                 required />
@@ -93,14 +93,47 @@
 
             <div class="tw-col-span-2">
               <label class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white">Note</label>
-              <textarea v-model="items.note" @input="note" rows="3"
+              <textarea v-model="items[0].note" @input="note" rows="3"
                 class="tw-bg-gray-50 tw-border tw-border-solid focus:tw-outline-none tw-border-gray-300 tw-text-gray-900 tw-text-sm tw-rounded-lg focus:tw-ring-orange-500 focus:tw-border-orange-500 tw-block tw-w-full tw-p-2.5 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-orange-500 dark:focus:tw-border-orange-500"
                 placeholder="Enter a note"></textarea>
 
             </div>
+            <div class="tw-col-span-2">
+                  <button @click="addAd" class="tw-py-2 tw-px-4 tw-bg-orange-400 tw-text-white tw-rounded">Add Ad</button>
+              </div>
           </div>
         </div>
-
+        <div class="tw-bg-gray-100 tw-rounded-lg tw-p-4 tw-shadow-md">
+        <h2 class="tw-text-lg tw-font-medium tw-mb-4">Added Ads</h2>
+        <table class="tw-min-w-full tw-divide-y tw-divide-gray-200">
+          <thead class="tw-bg-gray-50">
+            <tr>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Product ID</th>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Marketer</th>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Source</th>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Amount</th>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Leads</th>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Ads Date</th>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Note</th>
+              <th class="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase">Action</th>
+            </tr>
+          </thead>
+          <tbody class="tw-bg-white tw-divide-y tw-divide-gray-200">
+            <tr v-for="(item, index) in items.slice(1)" :key="index">
+              <td class="tw-px-4 tw-py-3 tw-whitespace-nowrap">{{ item.product_id }}</td>
+              <td class="tw-px-6 tw-py-4 tw-whitespace-nowrap">{{ getMarketerNameById(item.marketer_id) }}</td>
+              <td class="tw-px-4 tw-py-3 tw-whitespace-nowrap">{{ item.source }}</td>
+              <td class="tw-px-4 tw-py-3 tw-whitespace-nowrap">{{ item.amount }}</td>
+              <td class="tw-px-4 tw-py-3 tw-whitespace-nowrap">{{ item.leads }}</td>
+              <td class="tw-px-4 tw-py-3 tw-whitespace-nowrap">{{ item.ads_at }}</td>
+              <td class="tw-px-4 tw-py-3 tw-whitespace-nowrap">{{ item.note }}</td>
+              <td class="tw-px-4 tw-whitespace-nowrap">
+                <button @click="deleteAd(index + 1)" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-red-500/20 hover:tw-bg-red-500/10 hover:tw-border-red-500/70 tw-duration-300 tw-text-red-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center"><v-icon size="x-small">mdi-trash-can-outline</v-icon></button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
         <div
           class="tw-flex tw-justify-end tw-items-center tw-p-4 tw-gap-3 dark:tw-bg-neutral-900 tw-bg-gray-50 tw-border-t tw-border-solid">
           <button @click="cancel"
@@ -144,7 +177,7 @@
 
         products_fetched: null,
         products: [{id: 0, name: "Choose a product"}],
-        items: {
+        items: [{
           product_id: 0,
           marketer_id:0,
           source: 0,
@@ -152,16 +185,16 @@
           leads:0,
           ads_at: null,
           note: null
-        },
+        }],
 
-        errors: {
+        errors: [{
           product_id: null,
           marketer_id:null,
           source: null,
           amount: null,
           leads:null,
           ads_at: null,
-        },
+        }],
       };
     },
 
@@ -184,11 +217,58 @@
     },
 
     methods: {
-      handleCreate() {
-        const validated = validate(this, this.items);
-        if (!validated) return false;
+      getMarketerNameById(id) {
+        const marketer = this.marketers.find(m => m.id === id);
+        return marketer ? `${marketer.firstname} ${marketer.lastname}` : '';
+      },
+      deleteAd(index) {
+        this.items.splice(index, 1);
+      },
+      addAd() {
+      // Create a new ad object
+      const newAd = {
+        product_id: this.items[0].product_id,
+        marketer_id: this.items[0].marketer_id,
+        source: this.items[0].source,
+        amount: this.items[0].amount,
+        leads: this.items[0].leads,
+        ads_at: this.items[0].ads_at,
+        note: this.items[0].note,
+      };
+      if (this.items[0].product_id !== 0 && this.items[0].marketer_id !== 0 && this.items[0].source !== 0) {
+        this.items.push(newAd);
+        this.items[0] = {
+          product_id: 0,
+          marketer_id:0,
+          source: 0,
+          amount: 0,
+          leads:0,
+          ads_at: null,
+          note: null
+        };
+        this.errors[0] = {
+          product_id: null,
+          marketer_id:null,
+          source: null,
+          amount: null,
+          leads:null,
+          ads_at: null,
+        };
+      }
+    },
+    handleCreate() {
+        const ads = this.items.map((ad) => {
+          const validated = validate(this, ad);
+          if (!validated) return null;
+          return ad;
+        });
 
-        create(this, this.items);
+        // Filter out any null values from failed validations
+        const validAds = ads.filter(ad => ad !== null);
+
+        if (validAds.length > 0) {
+          create(this, validAds);
+        }
       },
 
       cancel() {
